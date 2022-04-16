a84258e5b6a1afd262b284256eb81184699c276a
Glitch (evanescent-wry-boar) <none>
2022-04-16 09:47:41 +0000

    🚧🚘 Checkpoint ./myApp.js:43926626/1184

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
 var apath_html = __dirname + "/views/index.html";
+var apath_assests = __dirname + "/public";
 function func(req, res) {
   res.sendFile(apath_html);
 }
 app.get("/", func);
-app.use("/public",middlewareFunction).
-
+app.use("/public", express.static(apath_assests));
 module.exports = app;
