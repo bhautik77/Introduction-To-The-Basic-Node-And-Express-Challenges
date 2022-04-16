4822c08f35439189c3bbc37b246c4a9f9eea7422
Glitch (evanescent-wry-boar) <none>
2022-04-16 10:22:49 +0000

    🍗👏 Checkpoint ./myApp.js:43926626/1699

--- myApp.js
+++ myApp.js
@@ -2,18 +2,25 @@ var express = require("express");
 var app = express();
 var apath_html = __dirname + "/views/index.html";
 var apath_assests = __dirname + "/public";
+
+function logger(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+}
+
 function func(req, res) {
   res.sendFile(apath_html);
 }
+
 function jsonapi(req, res) {
-  if (process.env.MESSAGE_STYLE=="uppercase"){
-    res.json({"message": "HELLO JSON"});
-  }
-  else {
-      res.json({"message": "Hello json"});
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
   }
 }
 app.get("/json", jsonapi);
+app.use("/", logger);
 app.get("/", func);
 app.use("/public", express.static(apath_assests));
 module.exports = app;
